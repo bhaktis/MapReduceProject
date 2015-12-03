46f24ad49b2f1a34f62768e95e749647fcc5fd5f
Deryl Rodrigues <drodrigues@dataxu.com>
2015-12-02 23:09:16 -0500

    [Deryl] Filtered all the columns required and performed join

--- YelpProject/1st.py
+++ YelpProject/1st.py
@@ -8,14 +8,16 @@ business = sqlContext.jsonFile("hdfs://localhost:54310/PYTHON/business.json")
 
 business.registerTempTable('business')
 
-b = sqlContext.sql("SELECT * FROM business")
+b = sqlContext.sql("SELECT business_id, name, neighborhoods, full_address, city, state, stars, categories FROM business")
+
+b.printSchema()
 
 c = b.filter(pyspark.sql.functions.array_contains(b.categories , "Restaurants"))
 
+business_rdd = c.map(lambda word: (word.business_id , [word.name, word.full_address, word.neighborhoods,
+                                   word.city, word.state, word.stars]))
 
-business_rdd = c.map(lambda word: (word.business_id , 1))
 
-# business_rdd.collect()
 
 
 #--------------------------------------- REVIEW --------------------
@@ -25,12 +27,44 @@ review = sqlContext.read.json("hdfs://localhost:54310/PYTHON/review.json")
 
 review.registerTempTable('review')
 
-r = sqlContext.sql("Select * from review")
+r = sqlContext.sql("Select business_id, stars, text from review")
 
-review_rdd = r.map(lambda word : (word.business_id , word.text))
+r.printSchema()
 
-# review_rdd.collect()
+review_rdd = r.map(lambda word : (word.business_id , [word.text, word.stars]))
 
 output = business_rdd.join(review_rdd)
 
 output.saveAsTextFile("file:///Users/drodrigues/output.txt")
+
+
+
+
+
+
+
+
+
+
+#b = business.select('business_id' , 'categories');
+
+# b.filter(pyspark.sql.functions.array_contains(b.categories , "Restaurants")).show()
+
+
+# review = sqlContext.read.json("hdfs://localhost:54310/PYTHON/r3.json")
+
+# r = review.select('business_id' , 'text')
+
+# j = b.join(r , b.business_id == r.business_id)
+
+# j.show()
+
+#joinData.show()
+
+#business_data = business.map(lambda x : x.getString("business_id"))
+
+#business_data.collect()
+
+#business.select(business['business_id' , business'name']).show()
+
+
